a1b2a14a643f0e2132d036624dd037c4cf354f8b
mayank <mayankjha997@gmail.com>
2024-04-13 16:10:31 +0530

    Initial commit

---
+++ src/main/java/com/vinishchoudhary/Todobackend/TodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vinishchoudhary.Todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinishchoudhary/Todobackend/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinishchoudhary.Todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
